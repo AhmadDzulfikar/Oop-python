64ed85a6990ed7620a1d42706265847b57c64d3f
Ahmad Dzulfikar <dzulfikar19.ahmad@gmail.com>
2023-07-03 15:29:42 +0700

    static and class method

---
+++ S&C_method_09/main.py
@@ -0,0 +1,34 @@
+# Static & Class Method
+
+class Hero:
+
+    # private class variable 
+    __jumlah = 0;
+
+    def __init__(self, name):
+        self.__name = name
+        Hero.__jumlah += 1
+
+    # method ini hanya berlaku untuk objek
+    def getJumlah(self):
+        return Hero.__jumlah
+    
+    # method ini tidak berlaku ke objek, hanya berlaku ke class
+    def getJumlah1():
+        return Hero.__jumlah
+    
+    # method static (decorator) nempel ke objek dan class
+    @staticmethod
+    def getJumlah2():
+        return Hero.__jumlah
+    
+    @classmethod
+    def getJumlah3(cls):
+        return cls.__jumlah
+    
+alu = Hero('Alucard')
+print(Hero.getJumlah2())
+vex = Hero('Vexana')
+print(vex.getJumlah2())
+gatot = Hero("Gatot Kaca")
+print(Hero.getJumlah3())
