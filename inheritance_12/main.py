29ce434f9276fbbde5020bf1ca9dde85459740da
Ahmad Dzulfikar <dzulfikar19.ahmad@gmail.com>
2023-07-04 22:11:56 +0700

    pendahuluan inheritance

---
+++ inheritance_12/main.py
@@ -0,0 +1,22 @@
+# inheritance = pewarisan 
+
+class Hero:
+
+    def __init__(self, name, health, power):
+        self.name = name
+        self.health = health    
+        self.power = power
+
+class Hero_Heal(Hero):
+    pass
+
+class Hero_damage(Hero):
+    pass
+
+sakura = Hero("sage sakura", 100, 20)
+sizune = Hero_Heal('sizune', 100, 20)
+A = Hero_damage('A', 600, 300)
+
+print(sakura.name)
+print(sizune.name)
+print(A.name)
